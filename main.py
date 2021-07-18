15622ae46513f22ad8c2e0377882d1f55a8ae6ec
Shami Ivan <sengashamiivan@yahoo.fr>
2021-07-17 22:31:20 -0400

    add Game object

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-print("The tic tac toe program is running")
-gameState = ["_"] * 9
-gameState[3]="X"
+class Game:
+    def __init__(self):
+       self.gameState = ["_"] * 9
+    
+    def displayGame(self):
+        matrix = [self.gameState[i:i+3] for i in range(0, len(self.gameState),3)]
+        for row in matrix:
+            print(*row,end="\n\n")
 
-matrix = [gameState[i:i+3] for i in range(0, len(gameState),3)]
-
-for row in matrix:
-    print(*row,end="\n\n")
+g=Game()
+g.displayGame()
