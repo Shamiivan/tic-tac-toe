e2ca94a074915c76fddd148be17452b4427038ff
Shami Ivan <sengashamiivan@yahoo.fr>
2021-07-17 22:08:27 -0400

    Displat grid represinting game state

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("The tic tac toe program is running")
+gameState = ["_"] * 9
+gameState[3]="X"
+
+matrix = [gameState[i:i+3] for i in range(0, len(gameState),3)]
+
+for row in matrix:
+    print(*row,end="\n\n")
