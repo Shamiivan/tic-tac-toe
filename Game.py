12adb22f973f0d0075f5126c7844fb694c8c3285
Shami Ivan <sengashamiivan@yahoo.fr>
2021-07-19 02:07:16 -0400

    Move Game class to separate file

---
+++ Game.py
@@ -0,0 +1,29 @@
+class Game:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.state = ["*"] * 9
+        self.counter = 0
+        self.winner = "none"
+ 
+    def display(self):
+        matrix = [self.state[i:i+3] for i in range(0,9,3)]
+        for row in matrix:
+            print(*row,end="\n\n")
+    
+    def checkWin(self):
+        lib = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        for arr in lib:
+            isWin = all(self.state.index == self.state.arr[0] for index in arr)
+        
+            if isWin: 
+                self.winner = self.state.arr[0]
+                self.end()
+    def checkDraw(self):
+        if self.counter == 8 :
+            self.end()
+    
+    def end(self):
+        print("game is over")
+
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-class Game:
-    def __init__(self):
-       self.gameState = ["_"] * 9
-    
-    def displayGame(self):
-        matrix = [self.gameState[i:i+3] for i in range(0, len(self.gameState),3)]
-        for row in matrix:
-            print(*row,end="\n\n")
+from Game import *
+
+
 
 g=Game()
-g.displayGame()
+g.state[4] = "X"
+g.display()
+print(g.counter)
