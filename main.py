c98f07370af27f87e7820fd97912ffac1c250355
Shami Ivan <sengashamiivan@yahoo.fr>
2021-07-22 19:38:04 -0400

    Add exceptions to handle bad user input

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ def startGame():
     game.display()
 
     def play():
-        move =int(input("What's your move : "))
+        while True :
+            try : 
+                move =int(input("What's your move : "))
+                if move > 8:
+                    raise
+                elif game.state[move] != "*":
+                    raise
+                else:break
+            except: 
+                print("Bad input")
         game.updateState(move)
         game.display()
 
     def endGame():
         print("Game Over \n","Winner : ", game.winner)
-        restart = input("Would you like to play again? y/n")
+        restart = input("Would you like to play again? y/n: ")
         if restart == "n":
             print("Twas a good game we should do it again")
             sys.exit()
